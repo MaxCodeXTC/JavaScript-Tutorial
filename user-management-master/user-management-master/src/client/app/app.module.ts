6b0d65898d58c7b64dac793a7ff23668a2988bf4
Radhika Subramanian <subramanian.radhika193@gmail.com>
2019-04-13 16:35:48 +0530

    New source added

---
+++ user-management-master/user-management-master/src/client/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { UserModule } from './user/user.module';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  imports: [
+  	BrowserModule, 
+    CoreModule,
+    AppRoutingModule,
+  	LoginModule,
+  	UserModule
+  ],
+  declarations: [
+  	AppComponent
+  ],
+  providers: [{
+    provide: APP_BASE_HREF,
+    useValue: '<%= APP_BASE %>',
+  }],
+  bootstrap: [
+  	AppComponent
+  ]
+})
+export class AppModule { }
